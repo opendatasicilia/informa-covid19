f3e9cb76c9747b632d17c431c5bc16361f012bac
Gabriele Scalici <gaba41@gmail.com>
2021-05-04 21:15:59 +0200

    default opengraph e meta component (#28)
    Add Open Graph
    
    Add Svelte Meta Component
    Add Social Card s

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,23 @@
 <script>
-    import Page from './components/Page.svelte';
+    import Meta from './components/Meta.svelte';
+    import Page from './components/Page.svelte'
+    import socialCardFacebook from './assets/informa-covid-social-card-fb.png'
+    import socialCardTwitter from './assets/informa-covid-social-card-tw.png'
+
+    const metadata = {
+      title: 'Informa Covid',
+      description:
+        'Buone pratiche di comunicazione pubblica dei dati COVID19 per le Amministrazioni Locali',
+      image: socialCardTwitter,
+      imageAlt: 'Informa Covid',
+      url: 'https://informacovid.opendatasicilia.it/',
+      type: 'article',
+      twitterCard: 'summary_large_image',
+      twitterHandler: '@opendatasicilia',
+    }
 </script>
 
+<Meta {metadata} />
 <Page />
 
 <style global lang='postcss'>
