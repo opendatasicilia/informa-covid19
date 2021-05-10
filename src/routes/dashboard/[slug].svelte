d3bcd576e6fe608d94df800914e6fd2e5ead614b
Gabriele Scalici <gaba@totel.it>
2021-05-10 21:19:12 +0200

    fixed mobile redirect

--- src/routes/dashboard/[slug].svelte
+++ src/routes/dashboard/[slug].svelte
@@ -1,34 +1,34 @@
 <script context="module">
-	export async function preload({ params }) {
-		const res = await this.fetch(`dashboard/${params.slug}.json`);
-		const data = await res.json();
-
-		if (res.status === 200) {
-			return { comune: data };
-		} else {
-			this.error(res.status, data.message);
-		}
+export async function preload({ params }) {
+	const res = await this.fetch(`dashboard/${params.slug}.json`);
+	const data = await res.json();
+	if (res.status === 200) {
+		return { comune: data };
+	} else {
+		this.error(res.status, data.message);
 	}
+}
 </script>
 
 <script>
 	export let comune;
-	import { stores } from '@sapper/app';
-    import UAParser  from 'ua-parser-js';
+	import { stores, goto } from '@sapper/app';
+	import { onMount } from 'svelte';
+	import UAParser  from 'ua-parser-js';
+	const { preloading, page, session } = stores();
+	var parser = new UAParser();
+	parser.setUA($session['user-agent']);
+	let mobile = parser.getResult().device['type'] == 'mobile';
 
-    const { preloading, page, session } = stores();
-    var parser = new UAParser();
-    parser.setUA($session['user-agent']);
-
-    let mobile = parser.getResult().device['type'] == 'mobile';
+	onMount(async () => {
+			if(mobile){
+				goto(comune.mobile)
+			}else{
+				goto(comune.desktop)
+			}
+		});
 </script>
 
 <svelte:head>
 	<title>Redirect a {comune.title}</title>
 </svelte:head>
-
-{#if mobile}
-	{window.location.href = comune.mobile}
-{:else}
-	{window.location.href = comune.desktop}
-{/if}
