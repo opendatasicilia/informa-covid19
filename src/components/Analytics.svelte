99ee3e4dee7f70690a87905fecc418ac8384d29d
Gabriele Scalici <gaba41@gmail.com>
2021-05-26 13:45:32 +0200

    Apply suggestions from code review
    Co-authored-by: Angelo Gulina <gulina.angelo@gmail.com>

--- src/components/Analytics.svelte
+++ src/components/Analytics.svelte
@@ -1,10 +1,10 @@
 <script>
-    const id = "UA-197920505-1";
+    const id = 'UA-197920505-1';
     import { stores } from '@sapper/app';
     const { page } = stores();
     $: {
-      if (typeof gtag !== "undefined"){
-        gtag("config", id, {
+      if (typeof gtag !== 'undefined') {
+        gtag('config', id, {
           page_path: $page.path
         });
       }
