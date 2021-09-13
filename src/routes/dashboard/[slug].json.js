2e4273cf97b4159e54b867eff9dd9d400372946d
Gabriele Scalici <gaba41@gmail.com>
2021-09-13 18:01:47 +0200

    redirect from gsheet

--- src/routes/dashboard/[slug].json.js
+++ src/routes/dashboard/[slug].json.js
@@ -1,11 +1,52 @@
-import comuni from './_comuni.js'
+let data, comune, comuni, columns, datarows
+const axios = require('axios')
 
 const lookup = new Map()
-comuni.forEach((comune) => {
-  lookup.set(comune.slug, JSON.stringify(comune))
-})
 
-export function get(req, res) {
+const gsheet =
+  'https://docs.google.com/spreadsheets/d/1eaKiVQhifRZpxqLh2vUCk4F7vm3nNP9nz67GrzArqtI/gviz/tq?tqx=out:json&sheet=csv_redirect'
+
+const fetchData = async () => {
+  axios
+    .get(gsheet)
+    .then((res) => {
+      data = JSON.parse(res.data.toString().substr(47).slice(0, -2))
+      data = data.table
+      const cols = data.cols
+      const rows = data.rows
+
+      columns = []
+      for (const col of cols) {
+        columns.push(col.label)
+      }
+
+      datarows = []
+      for (const row of rows) {
+        datarows.push(row.c.map((obj) => obj.v))
+      }
+
+      comuni = []
+      for (var y = 0; y < rows.length; y++) {
+        comune = {}
+        for (var i = 0; i < columns.length; i++) {
+          comune[columns[i]] = datarows[y][i]
+        }
+        comuni.push(comune)
+      }
+      return comuni
+    })
+    .then((comuni) => {
+      comuni.forEach((comune) => {
+        lookup.set(comune.slug, JSON.stringify(comune))
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+fetchData()
+
+export async function get(req, res) {
   const { slug } = req.params
 
   if (lookup.has(slug)) {
